71d11fccd71243a71256f5736766387982b36315
george <7jagjag@gmail.com>
2024-08-09 13:04:07 +0200

    great

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
-import { Button, Flex, Table, Thead, Th, Tr, Td, Tbody, Heading } from '@chakra-ui/react'
+import { Button, Flex, Table, Thead, Th, Tr, Td, Tbody, Heading, Container } from '@chakra-ui/react'
 import { useQuery } from '@tanstack/react-query'
 
 import { pb } from '@/services/pb'
@@ -11,40 +11,44 @@ export const Route = createFileRoute('/')({
 
 function Home() {
   const { data: sprints = [] } = useQuery({
-    queryKey: [Collections.SprintsView, 'get-all'],
-    queryFn: () => pb.collection(Collections.SprintsView).getFullList<SprintsViewResponse<number, number, number, number>>()
+    queryKey: [Collections.SprintsView, 'get-all', 'sort-sprint-desc'],
+    queryFn: () => pb.collection(Collections.SprintsView).getFullList<SprintsViewResponse<number, number, number, number>>({
+      sort: '-sprint',
+    })
   })
 
   return <Flex flexDir="column" gap="4" padding="4" background="whitesmoke" h="100dvh">
-    <Heading>Agile</Heading>
-    <Table size="sm" background="white" boxShadow="md" rounded="md">
-      <Thead>
-        <Tr background="purple.500">
-          <Th color="white" p="2">Sprint</Th>
-          <Th color="white">Points TBD</Th>
-          <Th color="white">Done Points</Th>
-          <Th color="white">To Val Points</Th>
-          <Th color="white">Points in Sprint</Th>
-          <Th color="white">Completed</Th>
-          <Th color="white">Actions</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {sprints.map(sprint => (
-          <Tr key={sprint.id}>
-            <Td>{sprint.sprint}</Td>
-            <Td>{sprint.tbd_points}</Td>
-            <Td>{sprint.done_points}</Td>
-            <Td>{sprint.to_val_points}</Td>
-            <Td>{sprint.total_points}</Td>
-            <Td>{Math.floor(100 * (sprint.done_points || 0) / (sprint.tbd_points || 1))} %</Td>
-            <Td display="flex" gap="2">
-              <Link to="/$sprintId/daily" params={{ sprintId: sprint.id }}><Button size="sm">view</Button></Link>
-              <Link><Button size="sm" disabled>staffing</Button></Link>
-            </Td>
+    <Container maxW="container.xl">
+      <Heading>Agile</Heading>
+      <Table size="sm" background="white" boxShadow="md" rounded="md">
+        <Thead>
+          <Tr background="purple.500">
+            <Th color="white" p="2">Sprint</Th>
+            <Th color="white">Points TBD</Th>
+            <Th color="white">Done Points</Th>
+            <Th color="white">To Val Points</Th>
+            <Th color="white">Points in Sprint</Th>
+            <Th color="white">Completed</Th>
+            <Th color="white">Actions</Th>
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {sprints.map(sprint => (
+            <Tr key={sprint.id}>
+              <Td>{sprint.sprint}</Td>
+              <Td>{sprint.tbd_points}</Td>
+              <Td>{sprint.done_points}</Td>
+              <Td>{sprint.to_val_points}</Td>
+              <Td>{sprint.total_points}</Td>
+              <Td>{Math.floor(100 * (sprint.done_points || 0) / (sprint.tbd_points || 1))} %</Td>
+              <Td display="flex" gap="2">
+                <Link to="/$sprintId/daily" params={{ sprintId: sprint.id }}><Button size="sm">view</Button></Link>
+                <Link><Button size="sm" disabled>staffing</Button></Link>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Container>
   </Flex>
 }
