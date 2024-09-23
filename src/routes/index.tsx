8046c80722daff9a09b438bfc87259e94702d5ac
george <7jagjag@gmail.com>
2024-09-23 10:35:38 +0200

    fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -60,7 +60,7 @@ function Home() {
     return _bySprint
   }, [sprintlabels])
 
-  const skip_sprints = ['Sprint 110', 'Sprint 111', 'Sprint 124']
+  const skip_sprints = ['Sprint 110', 'Sprint 111', 'Sprint 125']
 
   const problems = Object.keys(problemsBySprint)
     .filter((sprint) => !skip_sprints.includes(sprint))
