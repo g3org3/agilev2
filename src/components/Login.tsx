31489801fec00883ef5a6739de46f1d5e229e153
george <7jagjag@gmail.com>
2025-03-17 11:59:14 +0100

    better ui , new auth provider

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,6 @@
+import GithubIcon from '@/icons/GithubIcon'
+import GoogleIcon from '@/icons/GoogleIcon'
+import PassKeyIcon from '@/icons/PassKeyIcon'
 import { authenticateWithPasskey } from '@/services/authn'
 import { pb } from '@/services/pb'
 import { Collections, UsersResponse } from '@/services/pocketbase-types'
@@ -5,10 +8,18 @@ import { Button, Flex, Input, Text, useToast } from '@chakra-ui/react'
 import { useMutation } from '@tanstack/react-query'
 import { ClientResponseError } from 'pocketbase'
 import { usePostHog } from 'posthog-js/react'
-import { FormEventHandler } from 'react'
+import { FormEventHandler, useMemo } from 'react'
 
 export function Login() {
   const toast = useToast()
+  const isPassKeyAvailable = useMemo(() => {
+    return (
+      typeof window !== 'undefined' &&
+      window.PublicKeyCredential !== undefined &&
+      typeof window.PublicKeyCredential
+        .isUserVerifyingPlatformAuthenticatorAvailable === 'function'
+    )
+  }, [])
   const { mutate } = useMutation({
     mutationFn(username: string) {
       return authenticateWithPasskey(username.toString())
@@ -23,12 +34,12 @@ export function Login() {
     },
   })
   const posthog = usePostHog()
-  const onLogin = async () => {
+  const onLogin = (provider: 'google' | 'github') => async () => {
     let res = null
     try {
       res = await pb
         .collection(Collections.Users)
-        .authWithOAuth2<UsersResponse>({ provider: 'google' })
+        .authWithOAuth2<UsersResponse>({ provider })
     } catch (e) {
       const err = e as ClientResponseError
       alert(err.message)
@@ -61,36 +72,77 @@ export function Login() {
   return (
     <Flex alignItems="center" flexDirection="column" paddingTop="7%" gap={3}>
       <Flex
-        border="2px dotted black"
+        border="1px solid black"
+        shadow="md"
         py={10}
         px={20}
         borderColor="gray.200"
         flexDir="column"
+        rounded="md"
         gap={4}
       >
         <h1>
-          <Text fontSize="xx-large">Agile App</Text>
+          <Text letterSpacing="5px" fontSize="xxx-large">
+            Agile App
+          </Text>
         </h1>
-        <Button onClick={onLogin}>Login with Google</Button>
+        <Button
+          leftIcon={<GoogleIcon />}
+          _hover={{ bg: 'blue.500' }}
+          _active={{ bg: 'blue.600' }}
+          bg="blue.400"
+          color="white"
+          onClick={onLogin('google')}
+        >
+          Login with Google
+        </Button>
         <Flex alignItems="center" gap="2">
           <hr style={{ flex: 1 }} />
           or
           <hr style={{ flex: 1 }} />
         </Flex>
-        <form onSubmit={onAuthPasskey}>
-          <Flex gap="2">
-            <Input
-              type="text"
-              id="username"
-              name="username"
-              placeholder="Email"
-              autoComplete="username webauthn"
-            />
-            <Button name="login" px="10" type="submit">
-              Login PassKey
-            </Button>
-          </Flex>
-        </form>
+        <Button
+          leftIcon={<GithubIcon />}
+          _hover={{ bg: 'gray.600' }}
+          _active={{ bg: 'gray.500' }}
+          bg="black"
+          color="white"
+          onClick={onLogin('github')}
+        >
+          Login with Github
+        </Button>
+        {isPassKeyAvailable && (
+          <>
+            <Flex alignItems="center" gap="2">
+              <hr style={{ flex: 1 }} />
+              or
+              <hr style={{ flex: 1 }} />
+            </Flex>
+            <form onSubmit={onAuthPasskey}>
+              <Flex>
+                <Input
+                  type="text"
+                  id="username"
+                  name="username"
+                  placeholder="Email"
+                  autoComplete="username webauthn"
+                  borderRightRadius="none"
+                />
+                <Button
+                  borderLeftRadius="none"
+                  bg="purple.600"
+                  color="white"
+                  leftIcon={<PassKeyIcon />}
+                  name="login"
+                  px="10"
+                  type="submit"
+                >
+                  Login PassKey
+                </Button>
+              </Flex>
+            </form>
+          </>
+        )}
       </Flex>
     </Flex>
   )
