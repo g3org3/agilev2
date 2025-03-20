26037b2a060a6bf109e52508fb97f6232b40efc4
george <7jagjag@gmail.com>
2025-03-20 16:58:05 +0100

    fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -114,8 +114,8 @@ function Home() {
   const skipped_sprints = [
     'Sprint 110',
     'Sprint 111',
-    'Sprint 145',
-    'Datafeed - Sprint 145',
+    'Sprint 146',
+    'Datafeed - Sprint 146',
     'Datafeed - Sprint 1',
     'Datafeed - Sprint 2',
     'Datafeed - Sprint 3',
